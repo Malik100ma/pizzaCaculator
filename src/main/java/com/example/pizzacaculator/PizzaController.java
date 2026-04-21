17770b2241411c1421efab509ceceef095a62c2f
Malik <1malik1chen1@gmail.com>
2026-04-21 20:46:33 +0300

    add smaller logic

--- src/main/java/com/example/pizzacaculator/PizzaController.java
+++ src/main/java/com/example/pizzacaculator/PizzaController.java
@@ -1,14 +1,33 @@
 package com.example.pizzacaculator;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class PizzaController {
     @FXML
-    private Label welcomeText;
-
+    TextField diametrField1, diametrField2, priceField1, priceField2;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    Label resultLabel;
+
+    public void calculate(ActionEvent actionEvent) {
+        try {
+            double diameter1 = Double.parseDouble(diametrField1.getText());
+            double price1 = Double.parseDouble(diametrField1.getText());
+
+            double diameter2 = Double.parseDouble(diametrField1.getText());
+            double price2 = Double.parseDouble(diametrField1.getText());
+
+            double Value1 = calculateValua(diameter1, price1);
+            double Value2 = calculateValua(diameter2, price2);
+        } catch (NumberFormatException e) {
+            resultLabel.setText("Пишіть цифрами ");
+        }
+    }
+
+    public double calculateValua(double diameter, double price) {
+        double radius = diameter / 2;
+        return Math.PI * radius * radius / price;
     }
 }
